8ccd30eb9712444b1e92829fc7276b3c2a70b880
AesaKamar <aesa.kamar@gmail.com>
2018-04-24 15:54:18 -0400

    more passing tests

--- Day9/src/Lib.hs
+++ Day9/src/Lib.hs
@@ -1,24 +1,31 @@
 {-# LANGUAGE TypeFamilies #-}
 
-
 module Lib where
 
-import           Text.Parsec
-import           Text.Parsec.Char
-import           Text.Parsec.Combinator
-import           Text.Parsec.Prim
-import           Text.Parsec.String
+import Data.Monoid
+import Text.Parsec
+import Text.Parsec.Char
+import Text.Parsec.Combinator
+import Text.Parsec.Prim
+import Text.Parsec.String
 
 type RandomChars = String
 
-data Input = Garbage
-           | Group [Input]
-           deriving (Show, Eq)
-
-
+data Input
+  = Garbage String
+  | Group [Input]
+  deriving (Show, Eq)
 
 parserGarbage :: Parser Input
 parserGarbage = do
   _ <- string "<"
-  _ <- manyTill anyChar (string ">")
-  return Garbage
+  contents <- many normalChars
+  _ <- string ">"
+  return (Garbage $ concat contents)
+
+normalChars = (many1 validChars) <|> try (string "!>") <|> try (string "!!")
+  where
+    validChars =
+      oneOf
+        (['a' .. 'z'] <> ['A' .. 'Z'] <> ['0' .. '9'] <>
+         ['\'', '\"', '<', ' ', '{', '}'])
--- Day9/test/Spec.hs
+++ Day9/test/Spec.hs
@@ -1,33 +1,37 @@
 module Main where
 
-import           Control.Exception      (evaluate)
-import           Lib
-import           Test.Tasty.Hspec
-import           Text.Parsec
-import           Text.Parsec.Char
-import           Text.Parsec.Combinator
-import           Text.Parsec.Prim
-import           Text.Parsec.String
-
-
+import Control.Arrow (left)
+import Control.Exception (evaluate)
+import Lib
+import Test.Tasty.Hspec
+import Text.Parsec
+import Text.Parsec.Char
+import Text.Parsec.Combinator
+import Text.Parsec.Prim
+import Text.Parsec.String
 
 instance Eq ParseError where
-  (==) _ _ = False
+  (==) _ _ = True
 
 main :: IO ()
-main = hspec $ do
-  describe "Lookin for garbage" $ do
-    shouldParseAs "<>" parserGarbage (Right Garbage)
-    shouldParseAs "<random characters>" parserGarbage (Right Garbage)
-    shouldParseAs "<<<<>" parserGarbage (Right Garbage)
-    shouldParseAs "<{!>}>" parserGarbage (Right Garbage)
-    shouldParseAs "<!!>" parserGarbage (Right Garbage)
-    shouldParseAs "<!!!>>" parserGarbage (Right Garbage)
-    shouldParseAs "<{o\"i!a,<{i<a>" parserGarbage (Right Garbage)
+main =
+  hspec $ do
+    describe "Lookin for garbage" $ do
+      shouldParseAs "<>" parserGarbage (Right (Garbage ""))
+      shouldParseAs
+        "<random characters>"
+        parserGarbage
+        (Right (Garbage "random characters"))
+      shouldParseAs "<<<<>" parserGarbage (Right $ Garbage "<<<")
+      shouldParseAs "<!>>" parserGarbage (Right $ Garbage "!>")
+      shouldParseAs "<{!>}>" parserGarbage (Right $ Garbage "{!>}")
+      shouldParseAs "<!!>" parserGarbage (Right $ Garbage "!!")
+      shouldParseAs "<!!!>>" parserGarbage (Right $ Garbage "!!!>")
+      shouldParseAs
+        "<{o\"i!a,<{i<a>"
+        parserGarbage
+        (Right $ Garbage "{o\"i!a,<{i<a")
 
-
-
-
-
+-- Wow this is magical
 shouldParseAs inputString parser expectation =
   it inputString $ parse parser "" inputString `shouldBe` expectation
