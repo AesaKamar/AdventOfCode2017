ee8fd8c5bc2044d16c1190faf41301cf7bc5ef7c
AesaKamar <aesa.kamar@gmail.com>
2018-04-24 09:46:45 -0400

    parses garbage

--- Day9/src/Lib.hs
+++ Day9/src/Lib.hs
@@ -1,6 +1,24 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE TypeFamilies #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+
+module Lib where
+
+import           Text.Parsec
+import           Text.Parsec.Char
+import           Text.Parsec.Combinator
+import           Text.Parsec.Prim
+import           Text.Parsec.String
+
+type RandomChars = String
+
+data Input = Garbage
+           | Group [Input]
+           deriving (Show, Eq)
+
+
+
+parserGarbage :: Parser Input
+parserGarbage = do
+  _ <- string "<"
+  _ <- manyTill anyChar (string ">")
+  return Garbage
--- Day9/test/Spec.hs
+++ Day9/test/Spec.hs
@@ -1,2 +1,33 @@
+module Main where
+
+import           Control.Exception      (evaluate)
+import           Lib
+import           Test.Tasty.Hspec
+import           Text.Parsec
+import           Text.Parsec.Char
+import           Text.Parsec.Combinator
+import           Text.Parsec.Prim
+import           Text.Parsec.String
+
+
+
+instance Eq ParseError where
+  (==) _ _ = False
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Lookin for garbage" $ do
+    shouldParseAs "<>" parserGarbage (Right Garbage)
+    shouldParseAs "<random characters>" parserGarbage (Right Garbage)
+    shouldParseAs "<<<<>" parserGarbage (Right Garbage)
+    shouldParseAs "<{!>}>" parserGarbage (Right Garbage)
+    shouldParseAs "<!!>" parserGarbage (Right Garbage)
+    shouldParseAs "<!!!>>" parserGarbage (Right Garbage)
+    shouldParseAs "<{o\"i!a,<{i<a>" parserGarbage (Right Garbage)
+
+
+
+
+
+shouldParseAs inputString parser expectation =
+  it inputString $ parse parser "" inputString `shouldBe` expectation
