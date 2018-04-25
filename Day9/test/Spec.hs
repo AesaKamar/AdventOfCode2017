b14a3cfa40b0fcc59cb9fe5ab0769aee08b25a45
AesaKamar <aesa.kamar@gmail.com>
2018-04-25 11:11:26 -0400

    formatting

--- Day9/test/Spec.hs
+++ Day9/test/Spec.hs
@@ -1,14 +1,14 @@
 module Main where
 
-import           Control.Arrow          (left)
-import           Control.Exception      (evaluate)
-import           Lib
-import           Test.Tasty.Hspec
-import           Text.Parsec
-import           Text.Parsec.Char
-import           Text.Parsec.Combinator
-import           Text.Parsec.Prim
-import           Text.Parsec.String
+import Control.Arrow (left)
+import Control.Exception (evaluate)
+import Lib
+import Test.Tasty.Hspec
+import Text.Parsec
+import Text.Parsec.Char
+import Text.Parsec.Combinator
+import Text.Parsec.Prim
+import Text.Parsec.String
 
 instance Eq ParseError where
   (==) _ _ = True
@@ -78,6 +78,8 @@ main =
       "<!!!>>" `shouldCountNonEscapedCharsAs` 0
       "<{o\"i!a,<{i<a>" `shouldCountNonEscapedCharsAs` 10
 
+-- Wow these are magical
+-- ~Type Inference~
 shouldScoreAs inputString expectation =
   it inputString $
   (scoreGroups 1 <$> (parse parseGroups "" inputString)) `shouldBe`
@@ -94,6 +96,5 @@ shouldCountNonEscapedCharsAs inputString expectation =
   in it inputString $
      (countNonEscapedChars minusTwoOuters) `shouldBe` expectation
 
--- Wow this is magical
 shouldParseAs inputString parser expectation =
   it inputString $ parse parser "" inputString `shouldBe` expectation
