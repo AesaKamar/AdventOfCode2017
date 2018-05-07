7198e4af21d8acb85697d3eccf712b0455f98bfd
AesaKamar <aesa.kamar@gmail.com>
2018-05-07 11:52:41 -0400

    Incomplete Day10

--- Day10/src/Lib.hs
+++ Day10/src/Lib.hs
@@ -1,16 +1,41 @@
-module Lib  where
+module Lib where
 
-import           Data.Monoid
+import qualified Data.Boolean as B
+import qualified Data.List as L
+import Data.Monoid
+import qualified Data.Vector.Persistent as V
 
 lengthOfNumberString :: Int
 lengthOfNumberString = 256
 
-numberString :: [Int]
-numberString = [0..255]
+numberString :: V.Vector Int
+numberString = V.fromList [0 .. 255]
 
+applyNTimes n f xs = iterate f xs !! n
 
-tieKnot :: [Int] -> Int -> Int -> [Int]
-tieKnot numStr currentIndex skipAmount =
-  let
-    (first, second) = splitAt skipAmount numStr
-  in reverse first <> second
+-- tieKnot :: V.Vector Int -> Int -> Int -> V.Vector Int
+-- tieKnot numStr offset numToTake =
+--   let endOfGroup = (offset + numToTake)
+--       endOfNumStr = (V.length numStr)
+--       reversePart = V.reverse (V.slice offset endOfGroup numStr)
+--       theRest = V.slice endOfGroup endOfNumStr numStr
+--   in reversePart <> theRest
+indexBetween beginning ending len (idx, _) =
+  let cycledEnding =
+        if ending > len
+          then ending `mod` len
+          else ending
+  in if cycledEnding == beginning
+       then False
+       else if cycledEnding > beginning
+              then (idx <= cycledEnding) B.&& (idx >= beginning)
+              else B.not $ (idx < cycledEnding) `B.xor` (idx > beginning)
+
+-- Returns (IncludedInGroup, ExcludedInGroup)
+-- For some reason, the included group is already reversed
+partitionGroups :: Int -> Int -> [] (Int, a) -> ([] (Int, a), [] (Int, a))
+partitionGroups offset groupSize xs =
+  L.partition (indexBetween from to (L.length xs)) xs
+  where
+    from = offset
+    to = offset + groupSize
--- Day10/test/Spec.hs
+++ Day10/test/Spec.hs
@@ -1,10 +1,56 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Main where
 
-import           Lib
-import           Test.Tasty.Hspec
+import qualified Data.List as L
+import qualified Data.Vector.Persistent as V
+import Lib
+import Test.Tasty.Hspec
+import Text.Show.Pretty
 
 main :: IO ()
-main = hspec $ do
-  describe "Flipping a string" $ do
-    it "should tie with a single number" $
-      (tieKnot [0..4] 0 3) `shouldBe` [2, 1, 0, 3, 4]
+main =
+  hspec $
+  -- describe "Flipping a string" $ do
+  --   it "should tie with a single number" $
+  --     (tieKnot (V.fromList [0..4]) 0 3) `shouldBe`  V.fromList [2, 1, 0, 3, 4]
+  --   it "should tie with a single number with offset" $
+  --     (tieKnot (V.fromList [0..4]) 1 3) `shouldBe`  V.fromList [0, 3, 2, 1, 4]
+  --   it "should tie with a single number with offset" $
+  --     (tieKnot (V.fromList [0..4]) 2 3) `shouldBe`  V.fromList [0, 1, 4, 3, 2]
+  --   it "should tie with a single number with offset" $
+  --     (tieKnot (V.fromList [0..4]) 3 3) `shouldBe`  V.fromList [3, 1, 2, 0, 4]
+   do
+    describe "partitioning a string" $ do
+      t1
+      t2
+      t3
+      t4
+      t5
+  where
+    t1 =
+      let myVec = ([0 .. 4] `zip` [0 .. 4])
+      in it "partitionGroups 0 5 [0..4]" $
+         (partitionGroups 0 5 myVec) `shouldBe` (myVec, mempty)
+    t2 =
+      let myVec = ([0 .. 4] `zip` [0 .. 4])
+      in it "partitionGroups 0 0 [0..4]" $
+         (partitionGroups 0 0 myVec) `shouldBe` (mempty, myVec)
+    t3 =
+      let myVec = ([0 .. 4] `zip` [0 .. 4])
+          excludedSet = ([1, 2] `zip` [1, 2])
+          includedSet = ([3, 4, 0] `zip` [3, 4, 0])
+      in it "partitionGroups 3 3 [0..4]" $
+         (partitionGroups 3 3 myVec) `shouldBe` (includedSet, excludedSet)
+    t4 =
+      let myVec = ([0 .. 5] `zip` [0 .. 5])
+          excludedSet = ([2, 3] `zip` [2, 3])
+          includedSet = ([4, 5, 0, 1] `zip` [4, 5, 0, 1])
+      in it "partitionGroups 4 4 [0..5]" $
+         (partitionGroups 4 4 myVec) `shouldBe` (includedSet, excludedSet)
+    t5 =
+      let myVec = ([0 .. 5] `zip` [0 .. 5])
+          excludedSet = ([4, 5, 0, 1, 2] `zip` [4, 5, 0, 1, 2])
+          includedSet = ([3] `zip` [3])
+      in it "partitionGroups 3 1 [0..5]" $
+         (partitionGroups 3 1 myVec) `shouldBe` (includedSet, excludedSet)
