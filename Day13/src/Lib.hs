7e444b787c94cfd8578941a4e903b6402dc7b92f
AesaKamar <aesa.kamar@gmail.com>
2018-05-21 18:21:05 -0400

    tests and periodic function

--- Day13/src/Lib.hs
+++ Day13/src/Lib.hs
@@ -1,6 +1,27 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+
+import qualified Data.Array as Array
+import qualified Data.Ix    as Ix
+
+
+type IndexX = Integer
+type IndexY = Integer
+type Size = Integer
+
+
+
+
+data Column = Column IndexX Size
+
+
+
+
+moveSecurityBot :: Integer -> Size -> IndexY
+moveSecurityBot lengthOfArray picoseconds =
+  let splength = ((lengthOfArray-1) *2)
+      pos = picoseconds `mod` splength
+  in
+    if (pos < lengthOfArray) then pos
+    else splength - pos
--- Day13/test/Spec.hs
+++ Day13/test/Spec.hs
@@ -1,2 +1,18 @@
+module Main where
+
+import           Lib
+import           Test.Tasty
+import           Test.Tasty.Hspec
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "a test" $ do
+    it "in length 3, picosecond 3" $
+      moveSecurityBot 3 0 `shouldBe` 0
+    it "in length 3, picosecond 2" $
+      moveSecurityBot 3 2 `shouldBe` 2
+    it "in length 3, picosecond 3" $
+      moveSecurityBot 3 3 `shouldBe` 1
+    it "in length 5, picosecond 11" $
+      moveSecurityBot 5 11 `shouldBe` 3
