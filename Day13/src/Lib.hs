18e1d21db16b1ce91c86d02172cc9ddbed1839ed
AesaKamar <aesa.kamar@gmail.com>
2018-05-21 19:52:26 -0400

    checkpoint

--- Day13/src/Lib.hs
+++ Day13/src/Lib.hs
@@ -1,9 +1,8 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Lib where
 
-
-import qualified Data.Array as Array
-import qualified Data.Ix    as Ix
+import qualified Data.Array     as Array
+import qualified Data.Ix        as Ix
+import           Data.Semigroup ((<>))
 
 
 type IndexX = Integer
@@ -16,12 +15,26 @@ type Size = Integer
 data Column = Column IndexX Size
 
 
-
-
-moveSecurityBot :: Integer -> Size -> IndexY
+moveSecurityBot :: Size -> Integer -> IndexY
 moveSecurityBot lengthOfArray picoseconds =
+
   let splength = ((lengthOfArray-1) *2)
       pos = picoseconds `mod` splength
   in
-    if (pos < lengthOfArray) then pos
+    if pos < lengthOfArray
+    then pos
     else splength - pos
+
+
+moveScanner :: [IndexX] -> Integer -> [Maybe Column] -> [IndexX]
+moveScanner stucks pcs [] = stucks
+moveScanner stucks pcs (Nothing : restOfColumns) =
+  moveScanner stucks (pcs + 1) restOfColumns
+moveScanner stucks  pcs (Just (Column x len) : restOfColumns) =
+  let
+    securityBotPos = moveSecurityBot len pcs
+    scannerPos = 0
+  in
+    if scannerPos /= securityBotPos
+    then moveScanner stucks (pcs + 1) restOfColumns
+    else moveScanner (stucks <> [x]) (pcs + 1) (Just (Column x len) : restOfColumns)
--- Day13/test/Spec.hs
+++ Day13/test/Spec.hs
@@ -7,8 +7,8 @@ import           Test.Tasty.Hspec
 
 main :: IO ()
 main = hspec $ do
-  describe "a test" $ do
-    it "in length 3, picosecond 3" $
+  describe "security bots" $ do
+    it "in length 3, picosecond 0" $
       moveSecurityBot 3 0 `shouldBe` 0
     it "in length 3, picosecond 2" $
       moveSecurityBot 3 2 `shouldBe` 2
@@ -16,3 +16,13 @@ main = hspec $ do
       moveSecurityBot 3 3 `shouldBe` 1
     it "in length 5, picosecond 11" $
       moveSecurityBot 5 11 `shouldBe` 3
+  describe "moving the scanner" $ do
+    it "should move correctly" $
+      (moveScanner [] 0 columns) `shouldBe` [0, 6]
+      where
+        columns = [ Just (Column 0 3)
+                  , Just (Column 1 2)]
+                  -- , Nothing, Nothing
+                  -- , Just (Column 4 4)
+                  -- , Nothing
+                  -- , Just (Column 6 4)]
