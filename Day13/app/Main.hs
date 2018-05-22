dcc030de4aff391767a55be729fbdaf07cfaed96
AesaKamar <aesa.kamar@gmail.com>
2018-05-22 08:06:48 -0400

    solved part 1

--- Day13/app/Main.hs
+++ Day13/app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import           Data.List        (find)
+import           Data.Maybe       (catMaybes)
 import           Data.Traversable
 import           Lib
 import           System.IO
@@ -9,13 +10,14 @@ import           Text.Parsec.Prim
 main :: IO ()
 main = do
   lns <-  lines  <$> readFile "./input"
-  parsedCols <- pure $ traverse parseTheStuff lns
-  eitherIndexedColumns <- pure $ do
-    indexedColumns <- fmap (fmap (\c@( Column a b) -> (a, c))) parsedCols
-    pure $  makeIndexedList indexedColumns
-  stuckColumnIndicies <- pure $ moveScanner [] 0 <$> eitherIndexedColumns
-  stuckColumns <- pure $ fmap(findColumnsByIndex <$> parsedCols) stuckColumnIndicies
-  pure ()
+  res <- pure $ do
+    cols <-  traverse parseTheStuff lns
+    maybeIndexedCols <- pure $ makeIndexedList $ (\c@( Column a b) -> (a, c)) <$> cols
+    stuckColIndicies <- pure $ moveScanner [] 0 maybeIndexedCols
+    stuckCols <- pure $ findColumnsByIndex cols stuckColIndicies
+    severeties <- pure $ calculateSeverity  <$> (catMaybes stuckCols)
+    pure $ sum severeties
+  print res
 
 
 parseTheStuff str = parse columnParser "" str
