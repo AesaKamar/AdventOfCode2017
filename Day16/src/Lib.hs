959bffc874021efe2cd9296192d26bf2031178ce
AesaKamar <aesa.kamar@gmail.com>
2018-06-12 16:17:50 -0400

    spin function

--- Day16/src/Lib.hs
+++ Day16/src/Lib.hs
@@ -1,6 +1,16 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import           Control.Applicative
+import           Data.Array
+import           Data.Ix
+import           Data.List           (inits, tails)
+import           Data.Traversable    (sequenceA)
+
+type Structure i a = ((Array i i), (Array i a))
+
+arrLen = snd . bounds
+
+spin :: [a] -> Int -> [a]
+spin v offset = reverse $ take (length v) (drop offset (cycle $ reverse v))
+-- exchange :: [a] -> (a, a) -> [a]
+-- partner ::
--- Day16/test/Spec.hs
+++ Day16/test/Spec.hs
@@ -1,2 +1,12 @@
+module Main where
+
+import           Lib
+import           Test.Tasty
+import           Test.Tasty.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main =
+  hspec $ do
+    describe "spin" $ do
+      it "s0 abcde -> abcde" $ (spin "abcde" 0) `shouldBe` "abcde"
+      it "s3 abcde -> cdeab" $ (spin "abcde" 1) `shouldBe` "eabcd"
