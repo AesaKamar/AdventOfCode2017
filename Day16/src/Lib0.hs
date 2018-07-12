60c0ddcc4e3fe828944dc838c721d1caeb41b956
AesaKamar <aesa.kamar@gmail.com>
2018-07-12 16:53:18 -0400

    some changes

--- Day16/src/Lib0.hs
+++ Day16/src/Lib0.hs
@@ -6,111 +6,84 @@
 {-# LANGUAGE RecordWildCards      #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 
-module Lib0 where
+module Lib0
+where
 
 import           Control.Applicative
-import           Data.Char                            (toLower)
+import           Data.Char                      ( toLower )
 import           Data.Ix
-import           Data.List                            (elemIndex, inits, tails)
-import qualified Data.List.NonEmpty                   as NEL
-import qualified Data.Map                             as Map
-import           Data.Maybe                           (fromJust)
+import           Data.List                      ( elemIndex
+                                                , groupBy
+                                                , inits
+                                                , tails
+                                                )
+import qualified Data.List.NonEmpty            as NEL
+import qualified Data.Map                      as Map
+import           Data.Maybe                     ( fromJust )
 import           Data.Semigroup
-import           Data.Traversable                     (sequenceA)
-import           Data.Vector                          ((!), (//))
-import qualified Data.Vector                          as Vec
+import           Data.Traversable               ( sequenceA )
+import           Data.Vector                    ( (!)
+                                                , (//)
+                                                )
+import qualified Data.Vector                   as Vec
 import           Text.Parsec.Char
-import           Text.Parsec.String                   (Parser)
+import           Text.Parsec.String             ( Parser )
 import           Text.ParserCombinators.Parsec.Number
-
-data Letter
-  = A
-  | B
-  | C
-  | D
-  | E
-  | F
-  | G
-  | H
-  | I
-  | J
-  | K
-  | L
-  | M
-  | N
-  | O
-  | P
-  deriving (Eq, Show, Enum, Read)
 
 instance Semigroup PositionMappings where
   (<>) a b = (\(startA, endA) -> (startA, snd $ b ! endA)) <$> a
 
 type PositionMappings = Vec.Vector (Int, Int)
 
-type LetterNameMappings = Map.Map Letter Int
+type LetterNameMappings = Map.Map Char Int
 
-spin :: PositionMappings -> Int -> PositionMappings
-spin posMapping offset =
+spin :: Int -> PositionMappings -> PositionMappings
+spin offset posMapping =
   (\(startI, endI) -> (startI, cyclicShift endI)) <$> posMapping
-  where
-    cyclicShift endI = (endI - offset) `mod` length posMapping
+  where cyclicShift endI = (endI - offset) `mod` length posMapping
 
-exchange :: PositionMappings -> (Int, Int) -> PositionMappings
-exchange posMapping (i1, i2) =
+exchange :: (Int, Int) -> PositionMappings -> PositionMappings
+exchange (i1, i2) posMapping =
   posMapping // [(i1, posMapping ! i2), (i2, posMapping ! i1)]
 
-partner :: (Letter, Letter) -> LetterNameMappings -> LetterNameMappings
-partner (l1, l2) letMappingInit =
-  letMappingInit Map.\\
-  Map.fromList [(l1, letMappingInit Map.! l2), (l2, letMappingInit Map.! l1)]
+partner :: (Char, Char) -> LetterNameMappings -> LetterNameMappings
+partner (l1, l2) letMappingInit = letMappingInit Map.\\ Map.fromList
+  [(l1, letMappingInit Map.! l2), (l2, letMappingInit Map.! l1)]
 
-dancePositionMappings ::
-     LetterNameMappings -> NEL.NonEmpty PositionMappings -> LetterNameMappings
-dancePositionMappings lm pms = (\x -> snd $ finalPm ! x) <$> lm
-  where
-    finalPm = sconcat pms
+dance
+  :: (PositionMappings, LetterNameMappings)
+  -> [Move]
+  -> (PositionMappings, LetterNameMappings)
+dance (initPos, initLetters) [] = (initPos, initLetters)
+dance (initPos, initLetters) ((LetterIndependent newPos) : rest) =
+  dance (initPos <> newPos, initLetters) rest
+dance (initPos, initLetters) ((LetterDependent letterMappingFn) : rest) =
+  dance (initPos, letterMappingFn initLetters) rest
 
 data Move
-  = LetterDependent (PositionMappings -> LetterNameMappings)
-  | LetterIndependent PositionMappings
+  = LetterIndependent PositionMappings
+  | LetterDependent (LetterNameMappings -> LetterNameMappings)
 
-emptyPositionMapping16 = Vec.fromList [(x, x) | x <- [0 .. 15]]
+emptyPositionMapping16 = Vec.fromList [ (x, x) | x <- [0 .. 15] ]
 
 parseSpin :: Parser Move
 parseSpin = do
   s <- char 's'
   n <- int
-  pure $ LetterIndependent (spin emptyPositionMapping16 n)
+  pure $ LetterIndependent (spin n emptyPositionMapping16)
 
 parseExchange :: Parser Move
 parseExchange = do
-  x <- char 'x'
-  n1 <- int
+  x     <- char 'x'
+  n1    <- int
   slash <- char '/'
-  n2 <- int
-  pure $ LetterIndependent (exchange emptyPositionMapping16 (n1, n2))
-
-parseLetter :: Parser Letter
-parseLetter = do
-  (char 'a' >> pure A) <|> (char 'b' >> pure B) <|> (char 'c' >> pure C) <|>
-    (char 'd' >> pure D) <|>
-    (char 'e' >> pure E) <|>
-    (char 'f' >> pure F) <|>
-    (char 'g' >> pure G) <|>
-    (char 'h' >> pure H) <|>
-    (char 'i' >> pure I) <|>
-    (char 'j' >> pure J) <|>
-    (char 'k' >> pure K) <|>
-    (char 'l' >> pure L) <|>
-    (char 'm' >> pure M) <|>
-    (char 'n' >> pure N) <|>
-    (char 'o' >> pure O) <|>
-    (char 'p' >> pure P)
+  n2    <- int
+  pure $ LetterIndependent (exchange (n1, n2) emptyPositionMapping16)
 
 parsePartner :: Parser Move
 parsePartner = do
-  p <- char 'p'
-  l1 <- parseLetter
+  p     <- char 'p'
+  l1    <- letter
   slash <- char '/'
-  l2 <- parseLetter
-  pure $ LetterDependent ()
+  l2    <- letter
+  pure $ LetterDependent (partner (l1, l2))
--- Day16/test/Spec.hs
+++ Day16/test/Spec.hs
@@ -1,4 +1,5 @@
-module Main where
+module Main
+where
 
 import           Data.Array
 import           Lib
@@ -6,43 +7,50 @@ import           Test.Tasty
 import           Test.Tasty.Hspec
 
 main :: IO ()
-main =
-  hspec $ do
-    describe "spin" $ do
-      it "s0 abcde -> abcde" $
-        show
-          (spin
-             Structure
-             {cursor = 0, len = 5, letters = listArray (0, 4) [A .. E]}
-             0) `shouldBe`
-        "abcde"
-      it "s1 abcde -> eabcd" $
-        show
-          (spin
-             Structure
-             {cursor = 0, len = 5, letters = listArray (0, 4) [A .. E]}
-             1) `shouldBe`
-        "eabcd"
-      it "s3 abcde -> cdeab" $
-        show
-          (spin
-             Structure
-             {cursor = 0, len = 5, letters = listArray (0, 4) [A .. E]}
-             3) `shouldBe`
-        "cdeab"
-    describe "exchange" $ do
-      it "x3/4 eabcd -> eabdc" $
-        show
-          (exchange
-             Structure
-             {cursor = 0, len = 5, letters = listArray (0, 4) [E, A, B, C, D]}
-             (3, 4)) `shouldBe`
-        "eabdc"
-    describe "partner" $ do
-      it "pe/b eabdc -> baedc" $
-        show
-          (partner
-             Structure
-             {cursor = 0, len = 5, letters = listArray (0, 4) [E, A, B, D, C]}
-             (E, B)) `shouldBe`
-        "baedc"
+main = hspec $ do
+  describe "spin" $ do
+    it "s0 abcde -> abcde"
+      $          show
+                   (spin
+                     Structure {cursor = 0, len = 5, letters = listArray (0, 4) [A .. E]}
+                     0
+                   )
+      `shouldBe` "abcde"
+    it "s1 abcde -> eabcd"
+      $          show
+                   (spin
+                     Structure {cursor = 0, len = 5, letters = listArray (0, 4) [A .. E]}
+                     1
+                   )
+      `shouldBe` "eabcd"
+    it "s3 abcde -> cdeab"
+      $          show
+                   (spin
+                     Structure {cursor = 0, len = 5, letters = listArray (0, 4) [A .. E]}
+                     3
+                   )
+      `shouldBe` "cdeab"
+  describe "exchange" $ do
+    it "x3/4 eabcd -> eabdc"
+      $          show
+                   (exchange
+                     Structure
+                       { cursor  = 0
+                       , len     = 5
+                       , letters = listArray (0, 4) [E, A, B, C, D]
+                       }
+                     (3, 4)
+                   )
+      `shouldBe` "eabdc"
+  describe "partner" $ do
+    it "pe/b eabdc -> baedc"
+      $          show
+                   (partner
+                     Structure
+                       { cursor  = 0
+                       , len     = 5
+                       , letters = listArray (0, 4) [E, A, B, D, C]
+                       }
+                     (E, B)
+                   )
+      `shouldBe` "baedc"
